0e8e79b69077ac7adee2aceea98fb525ba49b43d
Rafael Macedo <rafaelmacedosilva88@hotmail.com>
2022-07-05 21:07:57 -0300

    Atualização nas rotas  e cofiguração do middleware

--- index.js
+++ index.js
@@ -1,40 +1,78 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');// Módulo nativo
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));// Middleware para leitura de arquivos estáticos
+app.use(express.urlencoded({extended: true}));
+// app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res, next) =>  { 
-    res.render('home');
-})
-
-app.get('/contato',(req,res) => {
-    res.render('contato');
-})
-
-app.listen(PORT, () => console.log('O servidor está rodando na port' + PORT));
-
+// query string  
+// http://localhost:3000/application?id=1&name=rafael
+// {
+//     "name": "rafael"
+// }
 
 
 
+// software do meio 
 
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.get('/', (req,res,next) => {
-//     res.send('<h2>Hello World 3</h2>');
+// A     PONTE     B
+// app.use((req,res,next) => {
+//     req.messagem = 'Eu não gosto de estudar';
+//     next();
 // })
 
-// app.listen(PORT, () => console.log('O servidor está rodando na port ' + PORT));
+// app.use((req,res,next) => {
+//     console.log(req.messagem);
+//     next();
+// })
 
 
+
+let users = []
+
+app.get('/', (req, res, next) =>  { 
+    res.render('home', {title: 'André desenvolvedor FullStack', apply: 'Job'});
+})
+
+app.get('/contact', (req, res) =>{
+    console.log(users);
+    res.render('contact');
+})
+
+app.post('/contact', (req, res) => {
+    const userData = {
+        name:  req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+    }
+    users.push(userData);
+    res.redirect('/users');
+})
+
+// app.get('/contact', (req,res) => {
+//     console.log(users);
+//     res.render('contact');
+// })
+
+// app.post('/contact', (req,res) => {
+//     const userData = {
+//         name: req.body.name,
+//         email: req.body.email,
+//         phone: req.body.phone
+//     }
+
+//     users.push(userData);
+//     res.redirect('/users');
+// })
+ 
+
+
+// Retorna os usuários
+app.get('/users', (req, res) => {
+    res.render('users', {users: users});
+})
+
+app.listen(PORT, () => console.log('O servidor está rodando na port: ' + PORT));
